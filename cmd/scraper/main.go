2125a9196da2a89380f527587cf60e71b3d77cb2
jeffrey1200 <jeffreymartespolanco@hotmail.com>
2024-06-19 14:00:59 -0400

    completely restructure the project and modified use of time.sleep for page.Race() instead as per official documentation

---
+++ cmd/scraper/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jeffrey1200/facebook-marketplace-scraper/config"
+	"github.com/jeffrey1200/facebook-marketplace-scraper/internals/browser"
+	"github.com/jeffrey1200/facebook-marketplace-scraper/internals/scraper"
+	"github.com/jeffrey1200/facebook-marketplace-scraper/util"
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	cfg := config.LoadConfig()
+
+	browserInstance, err := browser.NewBrowser(cfg.BrowserConfig, logger)
+	if err != nil {
+		logger.Fatal("Failed to launch browser", zap.Error(err))
+
+	}
+	defer browserInstance.Close()
+
+	carData, err := scraper.ScrapeMarketplace(browserInstance, cfg, logger)
+	if err != nil {
+		logger.Fatal("Failed to scrape marketplace", zap.Error(err))
+	}
+	util.SaveScrapedDataToJSON(carData)
+	log.Println("finished scraping!")
+}
